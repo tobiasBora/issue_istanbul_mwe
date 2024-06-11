9a4c6d0d5e25101c8c0d8d2cc617e737f1d8e39c
Tobias Bora <tobias.bora@gmail.com>
2024-06-11 02:06:10 +0200

    show issue with svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import istanbul from 'vite-plugin-istanbul';
+
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    sveltekit(),
+    istanbul({
+      include: 'src/*',
+      exclude: ['node_modules', 'tests/'],
+      extension: ['.js', '.ts', '.svelte'],
+      requireEnv: false
+    }),
+  ],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 });
